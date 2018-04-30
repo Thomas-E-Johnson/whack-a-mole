2ad98577c638fa94f39dabe4027824f2d2e543f7
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-04-30 15:40:45 +0100

    Final

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
   //math.random gives a number between 0 and 1
@@ -19,4 +21,32 @@ function randomHole(holes) {
   return hole;
 }
 
-//timestamp 6:19
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp)
+      peep();
+    }
+  , time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 10000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted)
+    return; //cheater!
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
